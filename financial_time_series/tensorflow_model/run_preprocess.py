ea327728a3e3a2ec7c75dbb3fb987599d13c2f83
DurivetMatthias <matthias.durivet@student.howest.be>
2019-03-04 10:27:43 +0100

    fixed pipeline, run_preprocess.py writes blob_path.txt

--- financial_time_series/tensorflow_model/run_preprocess.py
+++ financial_time_series/tensorflow_model/run_preprocess.py
@@ -1,54 +1,55 @@
 """Module for running the data retrieval and preprocessing.
-
 Scripts that performs all the steps to get the train and perform preprocessing.
 """
-import logging
 import argparse
-import shutil
+import logging
 import os
+import shutil
 
 from helpers import preprocess
 from helpers import storage as storage_helper
 
 
 def run_preprocess(args):
-  """Runs the retrieval and preprocessing of the data.
+    """Runs the retrieval and preprocessing of the data.
+    Args:
+      args: args that are passed when submitting the training
+    Returns:
+    """
+    tickers = ['snp', 'nyse', 'djia', 'nikkei',
+               'hangseng', 'ftse', 'dax', 'aord']
+    closing_data = preprocess.load_data(tickers, args.cutoff_year)
+    time_series = preprocess.preprocess_data(closing_data)
+    temp_folder = 'data'
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+    file_path = os.path.join(
+        temp_folder, 'data_{}.csv'.format(args.cutoff_year))
+    time_series.to_csv(file_path, index=False)
+    storage_helper.upload_to_storage(args.data_bucket, temp_folder)
 
-  Args:
-    args: args that are passed when submitting the training
-
-  Returns:
-
-  """
-  tickers = ['snp', 'nyse', 'djia', 'nikkei', 'hangseng', 'ftse', 'dax', 'aord']
-  closing_data = preprocess.load_data(tickers, args.cutoff_year)
-  time_series = preprocess.preprocess_data(closing_data)
-  temp_folder = 'data'
-  if not os.path.exists(temp_folder):
-    os.mkdir(temp_folder)
-  file_path = os.path.join(temp_folder, 'data_{}.csv'.format(args.cutoff_year))
-  time_series.to_csv(file_path, index=False)
-  storage_helper.upload_to_storage(args.data_bucket, temp_folder)
-  shutil.rmtree('data')
+    with open("/blob_path.txt", "w") as output_file:
+        output_file.write(file_path)
+    shutil.rmtree(temp_folder)
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Preprocessing')
+    parser = argparse.ArgumentParser(description='Preprocessing')
 
-  parser.add_argument('--data_bucket',
-                      type=str,
-                      help='GCS bucket where preprocessed data is saved',
-                      default='<your-bucket-name>')
+    parser.add_argument('--data_bucket',
+                        type=str,
+                        help='GCS bucket where preprocessed data is saved',
+                        default='<your-bucket-name>')
 
-  parser.add_argument('--cutoff_year',
-                      type=str,
-                      help='Cutoff year for the stock data',
-                      default='2010')
+    parser.add_argument('--cutoff_year',
+                        type=str,
+                        help='Cutoff year for the stock data',
+                        default='2010')
 
-  args = parser.parse_args()
-  run_preprocess(args)
+    args = parser.parse_args()
+    run_preprocess(args)
 
 
 if __name__ == '__main__':
-  logging.basicConfig(level=logging.INFO)
-  main()
+    logging.basicConfig(level=logging.INFO)
+    main()
--- financial_time_series/tensorflow_model/run_train.py
+++ financial_time_series/tensorflow_model/run_train.py
@@ -1,5 +1,4 @@
 """Module for running the training of the machine learning model.
-
 Scripts that performs all the steps to train the ML model.
 """
 import logging
@@ -15,137 +14,139 @@ from helpers import storage as storage_helper
 
 
 def run_training(args):
-  """Runs the ML model training.
+    """Runs the ML model training.
+    Args:
+      args: args that are passed when submitting the training
+    Returns:
+    """
+    # parse args
+    logging.info('parsing args...')
+    model = getattr(models, args.model)(nr_predictors=24, nr_classes=2)
 
-  Args:
-    args: args that are passed when submitting the training
+    # get the data
+    logging.info('getting the data...')
+    temp_folder = 'data'
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+    file_path = os.path.join(temp_folder, 'data.csv')
+    storage_helper.download_blob(args.data_bucket, args.blob_path, file_path)
+    time_series = pd.read_csv(file_path)
+    #time_series = args.data
+    training_test_data = preprocess.train_test_split(time_series, 0.8)
 
-  Returns:
+    # define training objective
+    logging.info('defining the training objective...')
+    sess = tf.Session()
+    feature_data = tf.placeholder("float", [None, 24])
+    actual_classes = tf.placeholder("float", [None, 2])
 
-  """
-  # parse args
-  logging.info('parsing args...')
-  model = getattr(models, args.model)(nr_predictors=24, nr_classes=2)
+    model = model.build_model(feature_data)
+    cost = -tf.reduce_sum(actual_classes * tf.log(model))
+    train_opt = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
-  # get the data
-  logging.info('getting the data...')
-  temp_folder = 'data'
-  if not os.path.exists(temp_folder):
-    os.mkdir(temp_folder)
-  file_path = os.path.join(temp_folder, 'data.csv')
-  storage_helper.download_blob(args.data_bucket, args.blob_path, file_path)
-  time_series = pd.read_csv(file_path)
-  training_test_data = preprocess.train_test_split(time_series, 0.8)
+    # train model
+    correct_prediction = tf.equal(
+        tf.argmax(model, 1), tf.argmax(actual_classes, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
+    logging.info('training the model...')
+    time_dct = {}
+    time_dct['start'] = time.time()
+    for i in range(1, args.epochs):
+        sess.run(
+            train_opt,
+            feed_dict={
+                feature_data: training_test_data['training_predictors_tf'].values,
+                actual_classes: training_test_data['training_classes_tf'].values.reshape(
+                    len(training_test_data['training_classes_tf'].values), 2)
+            }
+        )
+        if i % 5000 == 0:
+            print(i, sess.run(
+                accuracy,
+                feed_dict={
+                    feature_data: training_test_data['training_predictors_tf'].values,
+                    actual_classes: training_test_data['training_classes_tf'].values.reshape(
+                        len(training_test_data['training_classes_tf'].values), 2)
+                }
+            ))
+    time_dct['end'] = time.time()
+    logging.info('training took {0:.2f} sec'.format(
+        time_dct['end'] - time_dct['start']))
 
-  # define training objective
-  logging.info('defining the training objective...')
-  sess = tf.Session()
-  feature_data = tf.placeholder("float", [None, 24])
-  actual_classes = tf.placeholder("float", [None, 2])
+    # print results of confusion matrix
+    logging.info('validating model on test set...')
+    feed_dict = {
+        feature_data: training_test_data['test_predictors_tf'].values,
+        actual_classes: training_test_data['test_classes_tf'].values.reshape(
+            len(training_test_data['test_classes_tf'].values), 2)
+    }
+    metrics.tf_confusion_matrix(model, actual_classes, sess, feed_dict)
 
-  model = model.build_model(feature_data)
-  cost = -tf.reduce_sum(actual_classes * tf.log(model))
-  train_opt = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
-  init = tf.global_variables_initializer()
-  sess.run(init)
+    # create signature for TensorFlow Serving
+    logging.info('Exporting model for tensorflow-serving...')
 
-  # train model
-  correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(actual_classes, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-  logging.info('training the model...')
-  time_dct = {}
-  time_dct['start'] = time.time()
-  for i in range(1, args.epochs):
-    sess.run(
-        train_opt,
-        feed_dict={
-            feature_data: training_test_data['training_predictors_tf'].values,
-            actual_classes: training_test_data['training_classes_tf'].values.reshape(
-                len(training_test_data['training_classes_tf'].values), 2)
-        }
+    export_path = args.version
+    tf.saved_model.simple_save(
+        sess,
+        export_path,
+        inputs={'predictors': feature_data},
+        outputs={'prediction': tf.argmax(model, 1),
+                 'model-version': tf.constant([str(args.version)])}
     )
-    if i % 5000 == 0:
-      print(i, sess.run(
-          accuracy,
-          feed_dict={
-              feature_data: training_test_data['training_predictors_tf'].values,
-              actual_classes: training_test_data['training_classes_tf'].values.reshape(
-                  len(training_test_data['training_classes_tf'].values), 2)
-          }
-      ))
-  time_dct['end'] = time.time()
-  logging.info('training took {0:.2f} sec'.format(time_dct['end'] - time_dct['start']))
 
-  # print results of confusion matrix
-  logging.info('validating model on test set...')
-  feed_dict = {
-      feature_data: training_test_data['test_predictors_tf'].values,
-      actual_classes: training_test_data['test_classes_tf'].values.reshape(
-          len(training_test_data['test_classes_tf'].values), 2)
-  }
-  metrics.tf_confusion_matrix(model, actual_classes, sess, feed_dict)
+    # save model on GCS
+    logging.info("uploading to " + args.model_bucket + "/" + export_path)
+    storage_helper.upload_to_storage(args.model_bucket, export_path)
 
-  # create signature for TensorFlow Serving
-  logging.info('Exporting model for tensorflow-serving...')
-
-  export_path = args.version
-  tf.saved_model.simple_save(
-      sess,
-      export_path,
-      inputs={'predictors': feature_data},
-      outputs={'prediction': tf.argmax(model, 1),
-               'model-version': tf.constant([str(args.version)])}
-  )
-
-  # save model on GCS
-  logging.info("uploading to " + args.model_bucket + "/" + export_path)
-  storage_helper.upload_to_storage(args.model_bucket, export_path)
-
-  # remove local files
-  shutil.rmtree(export_path)
-  shutil.rmtree(temp_folder)
+    # remove local files
+    shutil.rmtree(export_path)
+    shutil.rmtree(temp_folder)
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Training')
+    parser = argparse.ArgumentParser(description='Training')
 
-  parser.add_argument('--model',
-                      type=str,
-                      help='model to be used for training',
-                      default='DeepModel',
-                      choices=['FlatModel', 'DeepModel'])
+    parser.add_argument('--model',
+                        type=str,
+                        help='model to be used for training',
+                        default='DeepModel',
+                        choices=['FlatModel', 'DeepModel'])
 
-  parser.add_argument('--epochs',
-                      type=int,
-                      help='number of epochs to train',
-                      default=30001)
+    parser.add_argument('--epochs',
+                        type=int,
+                        help='number of epochs to train',
+                        default=30001)
 
-  parser.add_argument('--version',
-                      type=str,
-                      help='version (stored for serving)',
-                      default='1')
+    parser.add_argument('--version',
+                        type=str,
+                        help='version (stored for serving)',
+                        default='1')
 
-  parser.add_argument('--data_bucket',
-                      type=str,
-                      help='GCS bucket where data is saved',
-                      default='<your-bucket-name>')
+    parser.add_argument('--data_bucket',
+                        type=str,
+                        help='GCS bucket where data is saved',
+                        default='<your-bucket-name>')
 
-  parser.add_argument('--blob_path',
+    parser.add_argument('--blob_path',
                       type=str,
                       help='GCS blob path where data is saved',
                       default='<your-bucket-name>')
 
-  parser.add_argument('--model_bucket',
-                      type=str,
-                      help='GCS bucket where model is saved',
-                      default='<your-bucket-name>')
+    parser.add_argument('--model_bucket',
+                        type=str,
+                        help='GCS bucket where model is saved',
+                        default='<your-bucket-name>')
 
-  args = parser.parse_args()
-  run_training(args)
+    parser.add_argument('--data',
+                        help='actual data output from previous step')
+
+    args = parser.parse_args()
+    run_training(args)
 
 
 if __name__ == '__main__':
-  logging.basicConfig(level=logging.INFO)
-  main()
+    logging.basicConfig(level=logging.INFO)
+    main()
